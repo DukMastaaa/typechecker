b1b40172d4c632f7aa107af8ea3ca86518ca5f3c
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2021-09-25 12:22:34 +1000

    implement dispatch system instead of if/elif

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typechecker import type_check
-from typing import List
+from typing import List, Dict, Tuple
 
 
 @type_check
@@ -23,10 +23,25 @@ def beans(numbers: List[int], multiplier: int) -> List[int]:
     return [multiplier * num for num in numbers]
 
 
+@type_check
+def sum_of_even_keys(d: Dict[int, int]) -> int:
+    return sum(value for key, value in d.items() if key % 2 == 0)
+
+
+@type_check
+def add_2d_vectors(vectors: List[Tuple[int, int]]) -> Tuple[int, int]:
+    return (sum(vec[0] for vec in vectors), sum(vec[1] for vec in vectors))
+
+
 if __name__ == "__main__":
-    print(add(1, 2))
-    foo = Foo(1)
-    print(meow(foo, 100))
+    # print(add(1, 2))
+    # foo = Foo(1)
+    # print(meow(foo, 100))
 
     # numbers = [1, 2, 3, 4, 5]
     # print(beans(numbers, 5))
+
+    # d = {1: 1, 2: 5, 3: 9, 4: 11}
+    # print(sum_of_even_keys(d))
+
+    print(add_2d_vectors([(1, 2), (2, 4), (3, 6), (4, 8)]))
--- typechecker.py
+++ typechecker.py
@@ -1,38 +1,101 @@
 import inspect
-from typing import Callable, Any, List, Tuple, Dict
+from typing import get_origin, get_args, Callable, Any, Literal, Tuple, Union, List
 
 
 def print_signature(function: Callable) -> None:
     print(inspect.signature(function))
 
 
-def check_equality(annotation: Any, value: Any) -> bool:
-    # probably won't deal with nested lists/tuples, rectify with recursive call
-    if annotation == List:
-        if not isinstance(value, list):
-            return False
-        for element in value:
-            if type(element) not in annotation.__args__:
-                return False
+_RECOGNISED_CONTAINER_TYPES = [tuple, list, dict, set]
 
-    elif annotation == Tuple:
-        if not isinstance(value, tuple):
-            return False
-        if len(value) != len(annotation.__args__):
-            return False
-        for index, element in enumerate(value):
-            if type(element) != annotation.__args__[index]:
+
+# I need to modify this after `check_equality` is defined because
+# the functions in here reference `check_equality`
+_TYPING_MODULE_DISPATCH_TABLE = dict()  # for annotations from the `typing` module
+_MISC_DISPATCH_TABLE = dict()  # for miscellaneous types not from `typing`
+
+
+def check_equality(value: Any, annotation: Any) -> bool:
+    # requires python >= 3.8
+    # https://docs.python.org/3/library/typing.html#typing.get_origin
+    # annotations from `typing` are now deprecated for python >= 3.9 but still works ._.
+    
+    origin = get_origin(annotation)
+    annotation_args = get_args(annotation)
+    
+    if origin is not None:
+        if origin in _RECOGNISED_CONTAINER_TYPES:
+            if not isinstance(value, origin):
                 return False
-    
-    elif annotation == Dict:
-        pass  # check key and value are of correct type
-    
+            elif len(annotation_args) == 0:
+                return True
+        return _TYPING_MODULE_DISPATCH_TABLE[origin](value, annotation_args)
     else:
-        if type(value) != annotation:
-            return False
-    
+        try:
+            return _MISC_DISPATCH_TABLE[annotation](value, annotation)
+        except KeyError:
+            return isinstance(value, annotation)
+
+
+def _handle_list(value: list, annotation_args: Tuple[Any]) -> bool:
+    if len(value) == 0:
+        return True
+    return all(check_equality(element, annotation_args[0]) for element in value)
+
+
+def _handle_tuple(value: tuple, annotation_args: Tuple[Any, ...]) -> bool:
+    if len(value) != len(annotation_args):
+        return False
+    return all(
+        check_equality(element, element_type)
+        for element, element_type in zip(value, annotation_args)
+    )
+
+
+def _handle_dict(value: dict, annotation_args: Tuple[Any, Any]) -> bool:
+    if len(value) == 0:
+        return True
+    key_annotation, value_annotation = annotation_args
+    return all(check_equality(key, key_annotation) for key in value.keys()) \
+        and all(check_equality(val, value_annotation) for val in value.values())
+
+
+def _handle_set(value: set, annotation_args: Tuple[Any]) -> bool:
+    # behaviour same
+    return _handle_list(value, annotation_args)
+
+
+def _handle_union(value: Any, annotation_args: Tuple[Any, ...]) -> bool:
+    return any(check_equality(value, annotation_type) for annotation_type in annotation_args)
+
+
+def _handle_literal(value: Any, annotation_args: Tuple[Any, ...]) -> bool:
+    return any(value == literal_element for literal_element in annotation_args)
+
+
+def _handle_any(value: Any, annotation_args: Tuple) -> bool:
     return True
-    
+
+
+def _handle_ellipsis(value: Any, annotation: Any) -> bool:
+    """Used for the ellipsis literal `...`"""
+    return value == ...
+
+
+_TYPING_MODULE_DISPATCH_TABLE.update({
+    list: _handle_list,
+    tuple: _handle_tuple,
+    dict: _handle_dict,
+    set: _handle_set,
+    Union: _handle_union,
+    Literal: _handle_literal,
+    Any: _handle_any
+})
+
+
+_MISC_DISPATCH_TABLE.update({
+    ...: _handle_ellipsis
+})
 
 
 def type_check(function: Callable) -> Callable:
@@ -45,14 +108,14 @@ def type_check(function: Callable) -> Callable:
                 continue
 
             value = args[index] if index < len(args) else kwargs[param.name]
-            if not check_equality(param.annotation, value):
+            if not check_equality(value, param.annotation):
                 raise TypeError(f"Expected {param.annotation} for parameter {param.name}, got {type(args[index])} instead")
 
         result = function(*args, **kwargs)
         if return_annotation != inspect.Signature.empty:
             if return_annotation is None and result is None:
                 pass
-            elif not check_equality(return_annotation, result):
+            elif not check_equality(result, return_annotation):
                 raise TypeError(f"Expected {return_annotation} as return value, got {type(result)} instead")
         return result
     
